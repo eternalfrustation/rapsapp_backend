04635f15c768b8f828fbd6928df9279b25349761
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-09-23 04:34:54 +0530

    gcloud compat things

--- config/db.ts
+++ config/db.ts
@@ -1,5 +1,18 @@
 import pgp from 'pg-promise'
+import { IConnectionParameters } from 'pg-promise/typescript/pg-subset';
 import process from 'process'
-export default pgp()(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`)
+console.log(process.env);
+const options: IConnectionParameters = {
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        database: process.env.POSTGRES_DATABASE,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PWD,
+        binary: true
+}
+
+console.log(options);
+
+export default pgp()(options)
 
 
--- sql/init/users.sql
+++ sql/init/users.sql
@@ -1,3 +1,13 @@
+DO $$
+BEGIN
+    BEGIN
+        CREATE TYPE user_role AS ENUM ('provider', 'client', 'admin');
+    EXCEPTION
+        WHEN duplicate_object THEN
+            RAISE NOTICE 'Type user_role already exists. Skipping creation.';
+    END;
+END
+$$;
 CREATE TABLE IF NOT EXISTS users (
 	id SERIAL PRIMARY KEY,
 	name text NOT NULL,
